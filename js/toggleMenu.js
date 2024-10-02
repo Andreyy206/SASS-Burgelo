cee669f5e5a65b196bddd60f569d0343f6b593ca
sibayarn <mysibayaneduardandreimaruquez@gmail.com>
2024-10-02 15:53:44 +0800

    master

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,6 +1,7 @@
 const burgir = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector("ul.header__nav");
 burgir.addEventListener("click", () => {
+    burgir.classList.toggle("open");
     headerNav.classList.toggle("open");
 
 })
